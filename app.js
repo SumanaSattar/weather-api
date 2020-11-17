09679e0207e6dd52f30c04cd7bfc6c742ac50493
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2020-11-17 12:29:15 +0100

    api weather

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-window.addEventListener('load',onloadFunction);
-function onloadFunction(){
-    let long;
-    let lat;
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position=> {
-            console.log(position);
-        });
-    } else {
-      console.log("it is not active");
+const api ={
+    key:"1e9ed597558a664923740217b013d342",
+    base:" https://api.openweathermap.org/data/2.5/"
+} 
+const cityName= document.querySelector("#cityName");
+cityName.addEventListener('keypress',setCity);
+
+function setCity(evt) {
+    if(evt.keyCode===13) {
+        getResults(cityName.value);
+        console.log(cityName.value);
     }
+}
+function getResults(query) {
+    fetch('${api.base})weather?q=${query}&units=metric&APPID=${api.key}')
+      .then(weather => {
+          return weather.json();
+      }).then(displayResults);
+}
+function displayResults(weather) {
+    console.log(weather);
 }
